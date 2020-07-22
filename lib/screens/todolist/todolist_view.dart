513ad83e3a13df54aa67d5801aec3fd548861613
Jumail <jumail@utm.my>
2020-07-23 00:32:48 +0800

    TodolistView has its own Provider

--- lib/screens/todolist/todolist_view.dart
+++ lib/screens/todolist/todolist_view.dart
@@ -14,61 +14,58 @@ class TodolistScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(context),
-      body: Selector<TodolistViewmodel, int>(
-        selector: (_, todolistViewmodel) => todolistViewmodel.todos?.length,
-        builder: (context, _, __) {
-          print('-' * 20);
+      body: ChangeNotifierProvider.value(
+        value: dependency<TodolistViewmodel>()
+          ..user = dependency<LoginViewmodel>().user,
+        child: Consumer<TodolistViewmodel>(
+          builder: (context, todolistViewmodel, __) {
+            print('-' * 20);
 
-          final todolistViewmodel =
-              Provider.of<TodolistViewmodel>(context, listen: false);
-          final todos = todolistViewmodel?.todos;
-          if (todos == null) {
-            return Center(child: CircularProgressIndicator());
-          }
+            final todos = todolistViewmodel?.todos;
+            if (todos == null) {
+              return Center(child: CircularProgressIndicator());
+            }
 
-          return ListView.separated(
-            itemCount: todos.length,
-            separatorBuilder: (context, index) => Divider(
-              color: Colors.blueGrey,
-            ),
-            itemBuilder: (context, index) {
-              return Selector<TodolistViewmodel, bool>(
-                selector: (_, todoListNotifier) =>
-                    todoListNotifier.todos[index].completed,
-                builder: (_, __, ___) {
-                  final todo =
-                      Provider.of<TodolistViewmodel>(context, listen: false)
-                          .todos[index];
+            return ListView.separated(
+              itemCount: todos.length,
+              separatorBuilder: (context, index) => Divider(
+                color: Colors.blueGrey,
+              ),
+              itemBuilder: (context, index) {
+                return Selector<TodolistViewmodel, bool>(
+                  selector: (_, todolistViewmodel) =>
+                      todolistViewmodel.todos[index].completed,
+                  builder: (_, __, ___) {
+                    final todo = dependency<TodolistViewmodel>().todos[index];
 
-                  // To show which ListTile gets rebuilt
-                  print('Build ListTile ${index + 1}');
+                    // To show which ListTile gets rebuilt
+                    print('Build ListTile ${index + 1}');
 
-                  return ListTile(
-                    title: Text(todo.title,
-                        style: TextStyle(
-                            decoration: todo.completed
-                                ? TextDecoration.lineThrough
-                                : TextDecoration.none)),
-                    subtitle: Text('id:  ${todo.id}'),
-                    onTap: () => todolistViewmodel.toggleTodoStatus(index),
-                    onLongPress: () => todolistViewmodel.removeTodo(index),
-                  );
-                },
-              );
-            },
-          );
-        },
+                    return ListTile(
+                      title: Text(todo.title,
+                          style: TextStyle(
+                              decoration: todo.completed
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none)),
+                      subtitle: Text('id:  ${todo.id}'),
+                      onTap: () => todolistViewmodel.toggleTodoStatus(index),
+                      onLongPress: () => todolistViewmodel.removeTodo(index),
+                    );
+                  },
+                );
+              },
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () =>
-            Provider.of<TodolistViewmodel>(context, listen: false).addNewTodo(),
+        onPressed: () => dependency<TodolistViewmodel>().addNewTodo(),
       ),
     );
   }
 
   AppBar _buildAppBar(BuildContext context) {
-    // final user = Provider.of<LoginViewmodel>(context, listen: false).user;
     final user = dependency<LoginViewmodel>().user;
 
     return AppBar(
@@ -82,8 +79,7 @@ class TodolistScreen extends StatelessWidget {
               color: Colors.red,
               size: 40),
           onPressed: () {
-            Provider.of<LoginViewmodel>(context, listen: false)
-                .selectUser(null);
+            dependency<LoginViewmodel>().selectUser(null);
             Navigator.pushReplacementNamed(
               context,
               router.loginRoute,
